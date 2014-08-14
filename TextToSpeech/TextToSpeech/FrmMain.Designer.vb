ab219b619a3217827596db179cf1e0952b0d6a72
ti-pich <apiche66@gmail.com>
2014-08-14 17:58:35 -0300

    Ajout de TextToSpeech

---
+++ TextToSpeech/TextToSpeech/FrmMain.Designer.vb
@@ -0,0 +1,50 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FrmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
+    'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnSpeak = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnSpeak
+        '
+        Me.btnSpeak.Location = New System.Drawing.Point(106, 187)
+        Me.btnSpeak.Name = "btnSpeak"
+        Me.btnSpeak.Size = New System.Drawing.Size(75, 23)
+        Me.btnSpeak.TabIndex = 0
+        Me.btnSpeak.Text = "Parler"
+        Me.btnSpeak.UseVisualStyleBackColor = True
+        '
+        'FrmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.btnSpeak)
+        Me.Name = "FrmMain"
+        Me.Text = "Voice Output"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents btnSpeak As System.Windows.Forms.Button
+
+End Class
---
+++ TextToSpeech/TextToSpeech/FrmMain.vb
@@ -0,0 +1,23 @@
+﻿Public Class FrmMain
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+
+
+    End Sub
+
+    Private Sub btnSpeak_Click(sender As Object, e As EventArgs) Handles btnSpeak.Click
+
+        Dim strHostName As String
+        Dim strIPAddress As String
+
+        strHostName = System.Net.Dns.GetHostName()
+
+        strIPAddress = System.Net.Dns.GetHostByName(strHostName).AddressList(0).ToString()
+
+        Dim SAPI
+        SAPI = CreateObject("SAPI.spvoice")
+        SAPI.speak(My.Computer.Name)
+
+    End Sub
+End Class
